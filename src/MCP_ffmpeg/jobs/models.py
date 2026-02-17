e7624abdd58afb76d7819b8619bd2baba1b3d3e7
priyanshu <mehtapriyanshu408@gmail.com>
2026-02-17 22:13:08 +0530

    models added

--- src/MCP_ffmpeg/jobs/models.py
+++ src/MCP_ffmpeg/jobs/models.py
@@ -3,7 +3,26 @@ This file will contain the models required for job manager
 """
 
 from enum import Enum
-
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Dict
 
 class JobAction(Enum):
     TRIM = "trim"
+
+
+class JobStatus(Enum):
+    QUEUED = "queued"
+    RUNNING =  "running"
+    SUCCESS = "success"
+    FAILED = "failed"
+
+
+@dataclass
+class JobDetails:
+    job_id: str
+    action: JobAction
+    params: Dict
+    status: JobStatus
+    created_at: datetime
+    updated_at: datetime
