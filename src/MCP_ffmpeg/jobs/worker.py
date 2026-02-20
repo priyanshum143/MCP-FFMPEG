48532c72f9d6e569943d776e6f464733220034a9
priyanshu <mehtapriyanshu408@gmail.com>
2026-02-20 07:34:03 +0530

    worker file added

---
+++ src/MCP_ffmpeg/jobs/worker.py
@@ -0,0 +1,11 @@
+"""
+This file contains the code to pick a task from jobs queue and execute it
+"""
+
+
+async def worker(job_id: str) -> None:
+    """
+    This method will contain
+    :param job_id:
+    :return:
+    """
