35214c27c8bbed6bba36082a791c720cc892fbb4
priyanshu <mehtapriyanshu408@gmail.com>
2026-02-22 10:25:47 +0530

    some variables added

--- src/MCP_ffmpeg/utils/variables.py
+++ src/MCP_ffmpeg/utils/variables.py
@@ -9,3 +9,7 @@ class CommonVariables:
     ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent
     OUTPUT_DIR = ROOT_DIR / "outputs"
     LOGS_DIR = ROOT_DIR / "logs"
+
+    JOB_DETAILS_JSON_FILE_NAME = "job_details.json"
+
+    PARALLEL_EXECUTIONS_ALLOWED = 1
